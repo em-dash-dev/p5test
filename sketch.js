d3a9d95c0fe8fd49e6e0ef0ca4c3b272ee012288
Emily Suber <54868242+em-dash-dev@users.noreply.github.com>
2022-06-15 11:56:19 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,6 @@ function setup() {
 function draw() {
   background(220);
   
-  let diameter = 200 * sin(0.01*frameCount);
+  let diameter = 200 * sin(0.001*millis());
   circle(width/2, height/2, diameter);
 }
